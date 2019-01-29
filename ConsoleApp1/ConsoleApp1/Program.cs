e49b9484ae5cc09cbf7210d552a32fcdb6a1c3c9
aamir <wasi3aamir@gmail.com>
2019-01-29 20:35:26 +0600

    Axxepted

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for(int t = 1; t <= T; t++)
+            {
+                Int32 sum = 0;
+                string line = Console.ReadLine();
+                string[] space = line.Split('\n');
+                Int32 num = Int32.Parse(space[0]);
+                sum = (num * (num + 1)) / 2;
+                Int32 result = (int)Math.Pow(sum, 2);
+                Console.WriteLine($"Case {t}: {result}");
+            }
+        }
+    }
+}
